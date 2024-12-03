771189024542f2b67d855a3ec7bba7854f6dba62
Artur Skrzypczyk <skrzypczyk.artur@gmail.com>
2024-12-03 08:01:07 +0100

    target sdk bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.plcoding.mastermeme"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.plcoding.mastermeme"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 

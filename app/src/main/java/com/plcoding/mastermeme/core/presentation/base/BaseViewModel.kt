e3c9986e35a8408c133ab5fe60d212fac887a563
Michal <michal.rabinski@vestiacom.com>
2024-12-06 20:55:09 +0100

    added default state method, removed passing default state from BaseViewModel's constructor

--- app/src/main/java/com/plcoding/mastermeme/core/presentation/base/BaseViewModel.kt
+++ app/src/main/java/com/plcoding/mastermeme/core/presentation/base/BaseViewModel.kt
@@ -3,15 +3,18 @@ package com.plcoding.mastermeme.core.presentation.base
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 
-abstract class BaseViewModel<UIState, UIEvent>(uiState: UIState) :
+abstract class BaseViewModel<UIState, UIEvent>() :
     ViewModel(),
     ViewModelInterface<UIState, UIEvent> {
+
+    abstract fun getDefaultState(): UIState
 
     /**
      * delegation for uiState,
      * so we don't need to do uiState.value = value, just uiState = value
      */
-    override var uiState: UIState by UIStateDelegate(mutableStateOf(uiState))
+    override var uiState: UIState by UIStateDelegate(mutableStateOf(getDefaultState()))
+
 
     /**
      * shortcut for setting value to state

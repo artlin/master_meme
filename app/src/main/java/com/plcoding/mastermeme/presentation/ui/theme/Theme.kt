3640ec882c21a0312df455c2f77ec97eb517dfa9
Michal <michal.rabinski@vestiacom.com>
2024-12-06 09:38:21 +0100

    moved ui elements into presentation root

--- app/src/main/java/com/plcoding/mastermeme/presentation/ui/theme/Theme.kt
+++ app/src/main/java/com/plcoding/mastermeme/presentation/ui/theme/Theme.kt
@@ -1,4 +1,4 @@
-package com.plcoding.mastermeme.ui.theme
+package com.plcoding.mastermeme.presentation.ui.theme
 
 import android.app.Activity
 import android.os.Build
--- app/src/main/java/com/plcoding/mastermeme/presentation/ui/theme/Type.kt
+++ app/src/main/java/com/plcoding/mastermeme/presentation/ui/theme/Type.kt
@@ -1,4 +1,4 @@
-package com.plcoding.mastermeme.ui.theme
+package com.plcoding.mastermeme.presentation.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle

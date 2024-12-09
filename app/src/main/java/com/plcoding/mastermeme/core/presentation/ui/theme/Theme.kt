b2a32daedbf934ea3be2e1bf2f099c2ed8574c0a
Michal <michal.rabinski@vestiacom.com>
2024-12-09 20:11:13 +0100

    added new theme color tokens

--- app/src/main/java/com/plcoding/mastermeme/core/presentation/ui/theme/Theme.kt
+++ app/src/main/java/com/plcoding/mastermeme/core/presentation/ui/theme/Theme.kt
@@ -14,13 +14,19 @@ import androidx.compose.ui.platform.LocalContext
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
     secondary = PurpleGrey80,
-    tertiary = Pink80
+    tertiary = Pink80,
+    surfaceContainerLow = CharcoalGray,
+    surfaceContainerLowest = DarkGreenBlack,
+    onSurface = PaleLavender,
 )
 
 private val LightColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
-    tertiary = Pink40
+    tertiary = Pink40,
+    surfaceContainerLow = CharcoalGray,
+    surfaceContainerLowest = DarkGreenBlack,
+    onSurface = PaleLavender,
 )
 
 @Composable
